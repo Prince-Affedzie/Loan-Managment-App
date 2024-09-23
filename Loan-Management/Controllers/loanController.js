c91abbb6a0c378d50c3aa3eac0b52a5ccba5df2e
Prince-Affedzie <paffedzie001@st.ug.edu.gh>
2024-09-23 22:34:56 +0000

    new changes

--- Loan-Management/Controllers/loanController.js
+++ Loan-Management/Controllers/loanController.js
@@ -8,18 +8,6 @@ const jwt = require('jsonwebtoken')
 
 const dashboard = async(req,res)=>{
   try{
-    const token = req.cookies.token;
-  if(!token){
-    return res.status(401).json({message:'No token provided'})
-  }
-  try{
-    const decoded = jwt.verify(token,process.env.JWT_SECRET);
-    req.user = decoded
-     
-  }catch(err){
-    console.log(err)
-    res.status(401).json({message:'Invalid token'})
-  }
     const findUser= await User.findById(req.user.id).populate('loan')
     if(!findUser){
       console.log('Could not find user')
