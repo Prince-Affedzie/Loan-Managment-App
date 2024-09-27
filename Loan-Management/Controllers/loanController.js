c2d18fc60cd87e46bc6482118db87abec3b65d4a
Prince-Affedzie <paffedzie001@st.ug.edu.gh>
2024-09-27 18:17:14 +0000

    redefined

--- Loan-Management/Controllers/loanController.js
+++ Loan-Management/Controllers/loanController.js
@@ -45,7 +45,8 @@ const applyForLoan = async (req, res) => {
       purpose
     });
     const savedLoan = await loan.save();
-    await User.findByIdAndUpdate(borrower,{loan:[...loan,savedLoan]})
+    const user = User.findById(borrower)
+    user.loan.push(savedLoan)
     //const userPhoneNumber = req.user.phoneNumber
     //sendSms( userPhoneNumber,'Your Loan Application has been submitted Succesfully')
     sendSMS(req.user.phoneNumber,"Hello your application for loan to Prostec Loans was successful")
