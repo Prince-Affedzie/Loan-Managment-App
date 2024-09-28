2b748ccc84846e22a08de14691958ffc7d1fe20b
Prince-Affedzie <paffedzie001@st.ug.edu.gh>
2024-09-28 12:20:48 +0000

    redefined

--- Loan-Management/Controllers/loanController.js
+++ Loan-Management/Controllers/loanController.js
@@ -277,7 +277,7 @@ const rejectedLoans = async (req, res) => {
 // When an admin wants to see all paid loans in the system
 const repaidLoans = async (req, res) => {
   try {
-    const loans = await Loan.find({ status: "fully paid",isArchived:false })
+    const loans = await Loan.find({ status: "fully paid",isArchived: false })
       .populate("borrower")
       .populate("repaymentSchedule");
     if (!loans) {
