810a966e017341d2b3c4556ac08052ed35c686c2
Prince-Affedzie <paffedzie001@st.ug.edu.gh>
2024-09-30 17:08:46 +0000

    redeined

--- Loan-Management/app.js
+++ Loan-Management/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const Repayment = require('./Models/repaymentModel')
 const CookieParser = require('cookie-parser')
 const path = require('path')
 const cors = require('cors')
@@ -29,7 +30,21 @@ mongoose.connect(process.env.MONGO_URI)
    .then(()=>app.listen(port, () => console.log(`Server is running on port ${port}`)))
    .catch((err) => console.log(err));
 
-   
+   const runMigrations = async () => {
+    try {
+      await Repayment.updateMany(
+        {}, 
+        { $set: { transactionId:" " } }
+      );
+      console.log('All users updated with default verification status');
+    } catch (err) {
+      console.error('Error during migration:', err);
+    }
+  };
+  
+  // Call the migration function during startup
+  runMigrations();
+  
    
 app.get('/', (req, res) => {
   res.send('Hello World!')
